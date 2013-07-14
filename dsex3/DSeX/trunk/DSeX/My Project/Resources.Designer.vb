db8e0fb023c00d87b2fa10484f81d3bcf177a834
Gerolkae_cp <SND\Gerolkae_cp>
2013-07-14 02:42:55 +0000

    Corrected SVN Tabbed Editors Section Broswing

--- dsex3/DSeX/trunk/DSeX/My Project/Resources.Designer.vb
+++ dsex3/DSeX/trunk/DSeX/My Project/Resources.Designer.vb
@@ -429,15 +429,5 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Icon)
             End Get
         End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property uparrow() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("uparrow", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
     End Module
 End Namespace
