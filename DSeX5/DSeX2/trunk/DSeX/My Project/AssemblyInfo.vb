1218dc6a8d3889e1faf447b4e9070748d9517dce
Gerolkae_cp <SND\Gerolkae_cp>
2013-08-14 11:07:54 +0000

    Added Libraries Folder and other needed files

--- DSeX5/DSeX2/trunk/DSeX/My Project/AssemblyInfo.vb
+++ DSeX5/DSeX2/trunk/DSeX/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.1.18.0813")> 
+<Assembly: AssemblyVersion("1.1.19.0814")> 
 <Assembly: AssemblyFileVersion("1.0.0.0")> 
