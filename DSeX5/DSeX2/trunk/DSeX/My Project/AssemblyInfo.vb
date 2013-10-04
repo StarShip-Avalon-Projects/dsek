89795aaa0e0cac8ad1cad6993fcbf07532c7a70a
Gerolkae_cp <SND\Gerolkae_cp>
2013-10-04 04:12:54 +0000

    Fixed Regex Escapes in Wizard fixed goto line off by 1 clear undo stacks on opening file or new file

--- DSeX5/DSeX2/trunk/DSeX/My Project/AssemblyInfo.vb
+++ DSeX5/DSeX2/trunk/DSeX/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("1.2.25.0921")> 
+<Assembly: AssemblyVersion("1.2.29.1003")> 
 <Assembly: AssemblyFileVersion("1.0.0.0")> 
